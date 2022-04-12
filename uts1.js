cde6d72136d55e20099fb5df2acd5c04edbdb836
imaderiskyputraprayoga@gmail.com <imaderiskyputraprayoga@gmail.com>
2022-04-12 15:43:52 +0800

    box mengikuti mouse

---
+++ uts1.js
@@ -0,0 +1,8 @@
+let box = document.getElementById("box");
+
+document.onmousemove = (event) => {
+    var x = event.clientX;
+    var y = event.clientY;
+    box.style.left = x - (box.clientWidth/2) + "px";
+    box.style.top = y - (box.clientWidth/2) + "px";
+}
