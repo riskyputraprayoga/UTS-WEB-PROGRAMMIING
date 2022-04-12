ec9124992879775b2a0ed5cf9b575fb22d97fa11
imaderiskyputraprayoga@gmail.com <imaderiskyputraprayoga@gmail.com>
2022-04-12 17:28:39 +0800

    box berubah warna random saat klik

--- uts1.js
+++ uts1.js
@@ -1,8 +1,22 @@
-let box = document.getElementById("box");
+let box = document.getElementById('box');
 
 document.onmousemove = (event) => {
     var x = event.clientX;
     var y = event.clientY;
     box.style.left = x - (box.clientWidth/2) + "px";
     box.style.top = y - (box.clientWidth/2) + "px";
-}
+    }
+
+const btn = document.getElementById('box');
+
+    let index = 0;
+    
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    
+    box.addEventListener('click', function onClick() {
+    box.style.backgroundColor = colors[index];
+    box.style.color = 'white';
+    
+    index = index >= colors.length - 1 ? 0 : index + 1;
+
+});
